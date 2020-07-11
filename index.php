2da0bbd3b51185990ffc6c528ca39967883631ba
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-07-11 17:05:19 +0500

    Content section 1

--- index.php
+++ index.php
@@ -22,6 +22,59 @@
             </div>
         </div>
     </section>
+
+    <section class="icons-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="info-box">
+                        <div class="info-icon info-icon-1">
+                            <img src="assets/svgs/connections.svg" alt="connections" class="ing-fluid">
+                        </div>
+                        <div class="info-text">
+                            <h4>Live Connection</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="info-box">
+                        <div class="info-icon info-icon-2">
+                            <img src="assets/svgs/engagements.svg" alt="connections" class="ing-fluid">
+                        </div>
+                        <div class="info-text">
+                            <h4>Live Engagement</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="info-box">
+                        <div class="info-icon info-icon-3">
+                            <img src="assets/svgs/community.svg" alt="connections" class="ing-fluid">
+                        </div>
+                        <div class="info-text">
+                            <h4>Live Community</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content-sec dotted-bg">
+        <img src="assets/svgs/bg-dots.svg" alt="" class="dotted-bg-img">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 md-order-2"><img src="assets/images/img-1.png" alt="" class="img-fluid"></div>
+                <div class="col-md-5 offset-md-1">
+                    <h2>Make Real Connections</h2>
+                    <p>Lorem ipsum dolor sit amet,<br class="d-none d-md-block"/> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+    </section>
     <?php include("includes/footer.php"); ?>
 </main>
 <?php include("includes/orientation-lock.php"); ?>
