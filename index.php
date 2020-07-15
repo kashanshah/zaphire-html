43f9896eb2732050643b2ebc8a6e7c199d8b62d0
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-07-16 03:09:01 +0500

    mods

--- index.php
+++ index.php
@@ -126,10 +126,8 @@
 							<div class="row">
 								<div class="col-md-12 text-center">
                                     <div id="jobseeker" class="tab-innr active">
-                                        <div class="image-div">
-                                            <img class="mb-4 img-fluid" src="assets/images/tab-1.jpg" alt="">
-                                            <h2>Lorem ipsum dolor sit</h2>
-                                        </div>
+    									<img class="mb-4 img-fluid" src="assets/images/tab-1.jpg" alt="">
+                                        <h2>Lorem ipsum dolor sit</h2>
                                         <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                                     </div>
                                     <div id="employers" class="tab-innr">
