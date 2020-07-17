77dae67315ae8e25d929cd9d36d113b4f1930be7
Kashan Shah <kashanshah@hotmail.com>
2020-07-18 03:40:24 +0500

    footer

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="info-box info-box-v1 animate" data-animation="fadeIn" data-duration="250">
+                    <div class="info-box info-box-v1 animate" data-animation="fadeIn" data-duration="5">
                         <div class="info-icon info-icon-1">
                             <img src="assets/svgs/connections.svg" alt="connections" class="ing-fluid">
                         </div>
@@ -43,7 +43,7 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="info-box info-box-v2 animate" data-animation="fadeIn" data-duration="250">
+                    <div class="info-box info-box-v2 animate" data-animation="fadeIn" data-duration="5">
                         <div class="info-icon info-icon-2">
                             <img src="assets/svgs/engagements.svg" alt="connections" class="ing-fluid">
                         </div>
@@ -54,7 +54,7 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="info-box info-box-v3 animate" data-animation="fadeIn" data-duration="250">
+                    <div class="info-box info-box-v3 animate" data-animation="fadeIn" data-duration="5">
                         <div class="info-icon info-icon-3">
                             <img src="assets/svgs/community.svg" alt="connections" class="ing-fluid">
                         </div>
@@ -222,12 +222,12 @@
                 </svg>
             </div>
             <div class="row align-items-center">
-                <div class="col-md-6 md-order-2 animate" data-animation="fadeIn" data-duration="250">
+                <div class="col-md-6 md-order-2 animate" data-animation="fadeIn" data-duration="5">
                     <img src="assets/images/img-1.png" alt="" class="img-fluid">
                 </div>
                 <div class="col-md-5 offset-md-1">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Make Real Connections</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet,<br class="d-none d-md-block"/> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Make Real Connections</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet,<br class="d-none d-md-block"/> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -235,7 +235,7 @@
     <section class="content-sec dotted-bg uppdots-sec">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="250">
+                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="5">
                 	<img src="assets/images/img-2.png" alt="" class="img-fluid">
                 	<ul class="circle-ul">
                 		<li></li>
@@ -243,8 +243,8 @@
                 	</ul>
                 </div>
                 <div class="col-md-5">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Immersive experience of hiring real people</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet,<br class="d-none d-md-block"/> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Immersive experience of hiring real people</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet,<br class="d-none d-md-block"/> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -252,10 +252,10 @@
     <section class="content-sec dotted-bg downdots-sec">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 order-md-0 text-md-right animate" data-animation="fadeIn" data-duration="250"><img src="assets/images/img-1.png" alt="" class="img-fluid"></div>
+                <div class="col-md-6 order-md-0 text-md-right animate" data-animation="fadeIn" data-duration="5"><img src="assets/images/img-1.png" alt="" class="img-fluid"></div>
                 <div class="col-md-6">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Believe the new Change</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Believe the new Change</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -265,9 +265,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h1 class="mb-5 text-center animate" data-animation="fadeIn" data-duration="250">Simplifying human <br class="d-none d-lg-block">expression in real-time</h1>
+                    <h1 class="mb-5 text-center animate" data-animation="fadeIn" data-duration="5">Simplifying human <br class="d-none d-lg-block">expression in real-time</h1>
                     <div class="tab-mn">
-                        <div class="tab-head animate" data-animation="fadeIn" data-duration="250">
+                        <div class="tab-head animate" data-animation="fadeIn" data-duration="5">
                             <ul>
                                 <li><a data-tag="jobseeker" class="active" href="javascript:;">Jobseeker</a></li>
                                 <li><a data-tag="employers" href="javascript:;">Employers</a></li>
@@ -275,7 +275,7 @@
                                 <li><a data-tag="coach" href="javascript:;">Coach</a></li>
                             </ul>
                         </div>
-                        <div class="tab-content animate" data-animation="fadeIn" data-duration="250">
+                        <div class="tab-content animate" data-animation="fadeIn" data-duration="5">
                             <div class="row">
                                 <div class="col-md-12 text-center">
                                     <div id="jobseeker" class="tab-innr active">
@@ -311,21 +311,21 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="box animate" data-animation="fadeIn" data-duration="250">
+                    <div class="box animate" data-animation="fadeIn" data-duration="5">
                         <img class="img-fluid" src="assets/images/box-01.png" alt="">
                         <h6>Virtual Career</h6>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="box animate" data-animation="fadeIn" data-duration="250">
+                    <div class="box animate" data-animation="fadeIn" data-duration="5">
                         <img class="img-fluid" src="assets/images/box-02.png" alt="">
                         <h6>Virtual Career</h6>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="box animate" data-animation="fadeIn" data-duration="250">
+                    <div class="box animate" data-animation="fadeIn" data-duration="5">
                         <img class="img-fluid" src="assets/images/box-03.png" alt="">
                         <h6>Virtual Career</h6>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
@@ -338,7 +338,7 @@
 	<section class="content-sec">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="250">
+                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="5">
                     <img src="assets/images/img-4.png" alt="" class="img-fluid">
                     <ul class="circle-ul circle-v2">
                         <li></li>
@@ -348,8 +348,8 @@
                     </ul>
                 </div>
                 <div class="col-md-6">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Reach</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Reach</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -358,12 +358,12 @@
     <section class="content-sec">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 order-md-0 text-md-right animate" data-animation="fadeIn" data-duration="250">
+                <div class="col-md-6 order-md-0 text-md-right animate" data-animation="fadeIn" data-duration="5">
                     <img src="assets/images/img-5.png" alt="" class="img-fluid">
                 </div>
                 <div class="col-md-6">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Co-host</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Co-host</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -372,7 +372,7 @@
     <section class="content-sec">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="250">
+                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="5">
                     <img src="assets/images/img-6.png" alt="" class="img-fluid">
                     <ul class="circle-ul circle-v2 v3">
                         <li></li>
@@ -381,8 +381,8 @@
                     </ul>
                 </div>
                 <div class="col-md-6">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Stream</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Stream</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -391,10 +391,10 @@
     <section class="content-sec">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 order-md-0 text-md-right animate" data-animation="fadeIn" data-duration="250"><img src="assets/images/img-5.png" alt="" class="img-fluid"></div>
+                <div class="col-md-6 order-md-0 text-md-right animate" data-animation="fadeIn" data-duration="5"><img src="assets/images/img-5.png" alt="" class="img-fluid"></div>
                 <div class="col-md-6">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Translate</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Translate</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -403,10 +403,10 @@
     <section class="content-sec">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="250"><img src="assets/images/img-7.png" alt="" class="img-fluid"></div>
+                <div class="col-md-6 order-md-1 animate" data-animation="fadeIn" data-duration="5"><img src="assets/images/img-7.png" alt="" class="img-fluid"></div>
                 <div class="col-md-6">
-                    <h2 class="animate" data-animation="fadeIn" data-duration="250">Ever live</h2>
-                    <p class="animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2 class="animate" data-animation="fadeIn" data-duration="5">Ever live</h2>
+                    <p class="animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
         </div>
@@ -415,11 +415,11 @@
     <section class="cta-sec">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 text-center animate" data-animation="fadeIn" data-duration="250">
+                <div class="col-md-12 text-center animate" data-animation="fadeIn" data-duration="5">
                     <div class="cta-mn">
-                        <h2 class=" animate" data-animation="fadeIn" data-duration="250">Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
+                        <h2 class=" animate" data-animation="fadeIn" data-duration="5">Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
                     </div>
-                    <a href="#" class="btn btn-primary animate" data-animation="fadeIn" data-duration="250"><span>Sign Up for Pilot Now</span></a>
+                    <a href="#" class="btn btn-primary animate" data-animation="fadeIn" data-duration="5"><span>Sign Up for Pilot Now</span></a>
                 </div>
             </div>
         </div>
