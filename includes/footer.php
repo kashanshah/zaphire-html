e8cf29cad72f262b1a53f909fbffcfa0c0ef461c
Abdul Basit Khan <abdulbasit0347@gmail.com>
2020-07-23 00:41:39 +0500

    changes

--- includes/footer.php
+++ includes/footer.php
@@ -31,3 +31,157 @@
 		</div>
 	</div>
 </footer>
+
+<div class="popup-mn popup-01">
+	<a href="javascript:;" class="close-btn"></a>
+		<div class="row no-gutters">
+		<div class="col-md-4">
+			<div class="left-side px-4 py-5 h-100 text-center">
+				<h5>Congratulations!</h5>
+				<p>You qualify for the Zaphire Live Pilot. To finish your enrollment, please share details about yourself:</p>
+			</div>
+		</div>
+		<div class="col-md-8 px-4 py-5">
+			<form action="">
+				<div class="form-group">
+					<label for="whtshouldcall">WHAT SHOULD WE CALL YOU?*</label>
+					<input type="text" class="form-control" id="whtshouldcall" placeholder="First Name here">
+				</div>
+				<div class="form-group">
+					<label for="whtshouldlastname">WHAT'S YOUR LAST NAME?*</label>
+					<input type="text" class="form-control" id="whtshouldlastname" placeholder="Last Name here">
+				</div>
+				<div class="form-group">
+					<label for="email">WE'LL EMAIL YOU BACK AS QUICKLY AS WE CAN*</label>
+					<input type="email" class="form-control" id="email" placeholder="Last Name here">
+				</div>
+				<div class="form-group">
+					<label for="country">WHERE ARE YOU FROM?</label>
+					<select class="form-control" id="country">
+						<option>Country</option>
+						<option>Country</option>
+						<option>Country</option>
+						<option>Country</option>
+						<option>Country</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<label for="maycallyou">WE MAY EVEN CALL YOU*</label>
+					<div class="d-flex">
+						<select class="form-control w-50" id="">
+							<option>Country Code</option>
+							<option>Country Code</option>
+							<option>Country Code</option>
+							<option>Country Code</option>
+							<option>Country Code</option>
+						</select>
+						<input type="text" class="form-control pl-3" id="maycallyou" placeholder="(888) - 888 -8888">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="whatdoyoudo">WHAT DO YOU DO?</label>
+					<input type="text" class="form-control" id="whatdoyoudo" placeholder="Tell us something about your profession">
+				</div>
+				<div class="form-group">
+					<label for="whatdolike">WHAT DO YOU LIKE?</label>
+					<select class="form-control" id="whatdolike">
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+					</select>
+				</div>
+				<div class="form-group mb-4">
+					<label for="industry">WHICH INDUSTRY DO YOU WORK IN?</label>
+					<select class="form-control" id="whatdolike">
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+					</select>
+				</div>
+				<input type="submit" value="Submit" class="btn btn-primary">
+			</form>
+		</div>
+	</div>
+</div>
+
+<div class="popup-mn popup-02">
+	<a href="javascript:;" class="close-btn"></a>
+		<div class="row no-gutters">
+		<div class="col-md-4">
+			<div class="left-side px-4 py-5 h-100 text-center">
+				<h5>Congratulations!</h5>
+				<p>You qualify for the Zaphire Live Pilot. To finish your enrollment, please share details about yourself:</p>
+			</div>
+		</div>
+		<div class="col-md-8 px-4 py-5">
+			<form action="">
+				<div class="form-group">
+					<label for="whtshouldcall">WHAT SHOULD WE CALL YOU?*</label>
+					<input type="text" class="form-control" id="whtshouldcall" placeholder="First Name here">
+				</div>
+				<div class="form-group">
+					<label for="whtshouldlastname">WHAT'S YOUR LAST NAME?*</label>
+					<input type="text" class="form-control" id="whtshouldlastname" placeholder="Last Name here">
+				</div>
+				<div class="form-group">
+					<label for="email">WE'LL EMAIL YOU BACK AS QUICKLY AS WE CAN*</label>
+					<input type="email" class="form-control" id="email" placeholder="Last Name here">
+				</div>
+				<div class="form-group">
+					<label for="country">WHERE ARE YOU FROM?</label>
+					<select class="form-control" id="country">
+						<option>Country</option>
+						<option>Country</option>
+						<option>Country</option>
+						<option>Country</option>
+						<option>Country</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<label for="maycallyou">WE MAY EVEN CALL YOU*</label>
+					<div class="d-flex">
+						<select class="form-control w-50" id="">
+							<option>Country Code</option>
+							<option>Country Code</option>
+							<option>Country Code</option>
+							<option>Country Code</option>
+							<option>Country Code</option>
+						</select>
+						<input type="text" class="form-control pl-3" id="maycallyou" placeholder="(888) - 888 -8888">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="whatdoyoudo">WHAT DO YOU DO?</label>
+					<input type="text" class="form-control" id="whatdoyoudo" placeholder="Tell us something about your profession">
+				</div>
+				<div class="form-group">
+					<label for="whatdolike">WHAT DO YOU LIKE?</label>
+					<select class="form-control" id="whatdolike">
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+						<option>Choose an interest</option>
+					</select>
+				</div>
+				<div class="form-group mb-4">
+					<label for="industry">WHICH INDUSTRY DO YOU WORK IN?</label>
+					<select class="form-control" id="whatdolike">
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+						<option>Choose an industry</option>
+					</select>
+				</div>
+				<input type="submit" value="Submit" class="btn btn-primary">
+			</form>
+		</div>
+	</div>
+</div>
+
+<div class="blackout"></div>
